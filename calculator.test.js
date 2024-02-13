28ea52812ce40bf09c4ece4974ad50bf79f6c77a
Sulaimanpandit <sulaiman.barnia@gmail.com>
2024-02-13 21:40:20 +0530

    new chages

--- calculator.test.js
+++ calculator.test.js
@@ -1,3 +1,11 @@
-describe('addTomemory', () => {
-  test('adds 1 + 2 to equal 3', () => {
-    // Test code here
+test('My first test case', () => {
+  console.log('Hello')
+});
+
+function addnum(num1,num2){
+  return num1+num2
+}
+test('add two number',() =>{
+  console.log('num3')
+expect(addnum(2,2)).toBe(4)
+});
