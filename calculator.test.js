12f0232ee6cc25e1b0a663ce76e28a4f5a605cda
Sulaimanpandit <sulaiman.barnia@gmail.com>
2024-02-13 17:31:41 +0530

    new chages

--- calculator.test.js
+++ calculator.test.js
@@ -1,3 +1,6 @@
-test('My first test case', () => {
-  console.log('Hello')
+const calculator = require.('./calculator);
+const x = 13;
+const y = 5;
+test(‘Add numbers using the add method’, () => {
+     expect(calculator.add(x, y)).toBe(18)
 });
