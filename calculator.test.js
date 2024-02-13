b17fb717cf145284a038c2ac1bc592460f46fa26
Sulaimanpandit <sulaiman.barnia@gmail.com>
2024-02-13 17:46:27 +0530

    new chages

--- calculator.test.js
+++ calculator.test.js
@@ -1,6 +1,4 @@
-const calculator = require.('./calculator);
-const x = 13;
-const y = 5;
-test(‘Add numbers using the add method’, () => {
-     expect(calculator.add(x, y)).toBe(18)
-});
+function add(a, b) {
+  return a + b;
+}
+module.exports.add = add;
