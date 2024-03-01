d74e6d00c562b56a84d31d94ae89f7d9728b0b66
Sulaimanpandit <sulaiman.barnia@gmail.com>
2024-03-01 23:08:49 +0530

    new chages

--- calculator.test.js
+++ calculator.test.js
@@ -9,3 +9,13 @@ test('add two number',() =>{
   console.log('num3')
 expect(addnum(2,2)).toBe(4)
 });
+test('3 plus 3 is 6', () => {
+  expect(3+ 3).toBe(6);
+});
+test('adding positive numbers is not zero', () => {
+  for (let a = 1; a < 10; a++) {
+    for (let b = 1; b < 10; b++) {
+      expect(a + b).not.toBe(0);
+    }
+  }
+});
